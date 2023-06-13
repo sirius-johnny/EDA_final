4408542eb9e2b0c4ed33bccbc25bb7a9be0493d9
Brian <braincheng101345@gmail.com>
2023-06-13 19:45:04 +0800

    Instance

--- ICCAD_B.cpp
+++ ICCAD_B.cpp
@@ -12,39 +12,72 @@
 using namespace std;
 
 int NumTechnologies;
-int DieSize_LL_X, DieSize_LL_Y, DieSize_UR_X, DieSize_UR_Y; 
+int DieSize_LL_X, DieSize_LL_Y, DieSize_UR_X, DieSize_UR_Y;
 int TopDieMaxUtil, BottomDieMaxUtil;
+typedef struct
+{
+    string pinName;
+    int pinLocationX, pinLocationY;
+} Pin;
+class Instance
+{
+    bool isMacro;
+    int libCellName, libCellSizeX, libCellSizeY, pinCount;
+    Pin *pin;
 
-
-int main(int argc, char* argv[]){
+    Instance(bool isMacro, int libCellName, int libCellSizeX, int libCellSizeY, int pinCount)
+    {
+        pin = new Pin[5];
+    }
+};
+int main(int argc, char *argv[])
+{
     fstream fin;
     fin.open("ProblemB_case1.txt", ios::in);
     fstream fout;
     fout.open("o.txt", ios::out);
 
-    if(fin){
+    if (fin)
+    {
         string lineStr;
-        while (getline(fin, lineStr)) {
+        while (getline(fin, lineStr))
+        {
             istringstream iss(lineStr);
             vector<string> words;
             string word;
 
-            while(iss>>word){
+            while (iss >> word)
+            {
                 words.push_back(word);
             }
-            
+
             // 一些規格的參數讀入
-            if(words[0]=="NumTechnologies"){NumTechnologies=stoi(words[1]);}
-            if(words[0]=="DieSize"){DieSize_LL_X=stoi(words[1]);DieSize_LL_Y=stoi(words[2]);DieSize_UR_X=stoi(words[3]);DieSize_UR_Y=stoi(words[4]);}
-            if(words[0]=="TopDieMaxUtil"){TopDieMaxUtil=stoi(words[1]);}
-            if(words[0]=="BottomDieMaxUtil"){BottomDieMaxUtil=stoi(words[1]);}
-            
-            
-            for (const auto& w : words) {
+            if (words[0] == "NumTechnologies")
+            {
+                NumTechnologies = stoi(words[1]);
+            }
+            if (words[0] == "DieSize")
+            {
+                DieSize_LL_X = stoi(words[1]);
+                DieSize_LL_Y = stoi(words[2]);
+                DieSize_UR_X = stoi(words[3]);
+                DieSize_UR_Y = stoi(words[4]);
+            }
+            if (words[0] == "TopDieMaxUtil")
+            {
+                TopDieMaxUtil = stoi(words[1]);
+            }
+            if (words[0] == "BottomDieMaxUtil")
+            {
+                BottomDieMaxUtil = stoi(words[1]);
+            }
+
+            for (const auto &w : words)
+            {
                 cout << w << endl;
             }
 
-            cout<<"---------------"<<endl;
+            cout << "---------------" << endl;
         }
     }
 }
